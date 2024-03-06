3a7f1245caecc6faf33dc95da477e1096ace0957
fassal farah <farahfassal32@gmail.com>
2024-03-05 16:43:37 -0800

    add

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    print(r.text) if status < 400 else print(
+        "Error code: {}".format(r.status_code))
